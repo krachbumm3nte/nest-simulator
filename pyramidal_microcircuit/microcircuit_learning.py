2d91ddc8c7bf014ab56dd5a663e030948966f13e
johannes gille <johannes.gille@gmx.de>
2023-03-18 10:18:13 +0100

    extract plotting code to separate file

--- pyramidal_microcircuit/microcircuit_learning.py
+++ pyramidal_microcircuit/microcircuit_learning.py
@@ -1,21 +1,15 @@
-import nest
-import matplotlib.pyplot as plt
 import numpy as np
 from networks.network_nest import NestNetwork
 from networks.network_numpy import NumpyNetwork
-from sklearn.metrics import mean_squared_error as mse
 from time import time
 import utils
 import os
 import json
-import sys
 import argparse
-import pandas as pd
 from datetime import timedelta
 from networks.params import *  # nopep8
 import warnings
 warnings.simplefilter('error', RuntimeWarning)
-
 
 
 parser = argparse.ArgumentParser()
@@ -39,7 +33,7 @@ parser.add_argument("--weights",
                     help="Start simulations from a given set of weights to ensure comparable results.")
 parser.add_argument("--plot",
                     type=int,
-                    default=50,
+                    default=0,
                     help="generate a plot of training progress after every n epochs.")
 parser.add_argument("--config",
                     type=str,
@@ -92,20 +86,17 @@ if args.weights:
 
 dims = net.dims
 
-cmap_1 = plt.cm.get_cmap('hsv', dims[1]+1)
-cmap_2 = plt.cm.get_cmap('hsv', dims[2]+1)
-
 
 simulation_times = []
 if args.cont:
     net.test_acc = progress["test_acc"]
     net.test_loss = progress["test_loss"]
     net.train_loss = progress["train_loss"]
-    net.V_ah_record = np.array(progress["V_ah_record"])
-    net.U_y_record = np.array(progress["U_y_record"])
-    net.U_i_record = np.array(progress["U_i_record"])
-    ff_error = progress["ff_error"]
-    fb_error = progress["fb_error"]
+    # net.V_ah_record = np.array(progress["V_ah_record"])
+    # net.U_y_record = np.array(progress["U_y_record"])
+    # net.U_i_record = np.array(progress["U_i_record"])
+    # ff_error = progress["ff_error"]
+    # fb_error = progress["fb_error"]
     epoch_offset = progress["epochs_completed"]
     net.epoch = epoch_offset
     print(f"continuing training from epoch {epoch_offset}")
@@ -121,13 +112,14 @@ if not args.cont:
     utils.store_synaptic_weights(net, root_dir, "init_weights.json")
 
 print("setup complete, running simulations...")
-plot_every = args.plot
-start_training = time()
+plot_interval = args.plot
+if plot_interval > 0:
+    from plot_utils import plot_progress
+
 
 # if spiking:
 #     sr = nest.Create("spike_recorder")
 #     nest.Connect(nest.GetNodes({"model": params.neuron_model}), sr)
-
 
 try:  # catches KeyboardInterruptException to ensure proper cleanup and storage of progress
     t_start_training = time()
@@ -153,86 +145,12 @@ try:  # catches KeyboardInterruptException to ensure proper cleanup and storage 
             if epoch > 0:
                 t_processed = time() - t_start_training
                 t_epoch = t_processed / epoch
-                print(f"\tETA: {timedelta(seconds=np.round(t_epoch * (params.n_epochs-epoch)))}\n")
+                print(f"\t epoch time: {np.mean(simulation_times[-50:]):.2f}s, ETA: {timedelta(seconds=np.round(t_epoch * (params.n_epochs-epoch)))}\n")
 
-        if plot_every > 0 and epoch % plot_every == 0:
-            print(f"Ep {epoch}: generating plot...")
+        if plot_interval > 0 and epoch % plot_interval == 0:
+            plot_progress(epoch, net, imgdir)
 
-            plt.rcParams['savefig.dpi'] = 300
 
-            fig, axes = plt.subplots(4, 2, constrained_layout=True)
-            [ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7] = axes.flatten()
-
-            intn_error = np.square(net.U_y_record - net.U_i_record)
-
-            mean_error = utils.rolling_avg(np.mean(intn_error, axis=1), size=200)
-            ax0.plot(mean_error, color="black")
-
-            intn_error_now = np.mean(mean_error[-20:])
-            ax0_2 = ax0.secondary_yaxis("right")
-            ax0_2.set_yticks([intn_error_now])
-
-            apical_error = np.linalg.norm(net.V_ah_record, axis=1)
-            ax1.plot(utils.rolling_avg(apical_error, size=150))
-
-            apical_error_now = np.mean(apical_error[-20:])
-            ax1_2 = ax1.secondary_yaxis("right")
-            ax1_2.set_yticks([apical_error_now])
-
-            # Synaptic weights
-            # notice that all weights are scaled up again to ensure that derived metrics are comparible between simulations
-            weights = net.get_weight_dict()
-            WHI = weights[-2]["pi"]
-            WHY = weights[-2]["down"]
-            WIH = weights[-2]["ip"]
-            WYH = weights[-1]["up"]
-
-            fb_error_now = mse(WHY.flatten(), -WHI.flatten())
-            fb_error.append([epoch, fb_error_now])
-
-            ff_error_now = mse(WYH.flatten(), WIH.flatten())
-            ff_error.append([epoch, ff_error_now])
-
-            # ax2.plot(*zip(*fb_error), label=f"FB error: {fb_error_now:.3f}")
-            ax2.plot(*zip(*net.train_loss))
-            ax3.plot(*zip(*ff_error), label=f"FF error: {ff_error_now:.3f}")
-
-            # plot synaptic weights
-            for i in range(dims[2]):
-                col = cmap_2(i)
-                for j in range(dims[1]):
-                    ax4.plot(j, -WHY[j, i], ".", color=col, label=f"to {i}")
-                    ax4.plot(j, WHI[j, i], "x", color=col, label=f"from {i}")
-
-            for i in range(dims[1]):
-                for j in range(dims[2]):
-                    col = cmap_2(j)
-                    ax5.plot(i, WYH[j, i], ".", color=col, label=f"to {i}")
-                    ax5.plot(i, WIH[j, i], "x", color=col, label=f"from {i}")
-
-            ax6.plot(*zip(*net.test_acc))
-            ax7.plot(*zip(*net.test_loss))
-
-            ax0.set_title("interneuron - pyramidal error")
-            ax1.set_title("apical error")
-            ax2.set_title("Feedback error")
-            ax3.set_title("Feedforward error")
-            ax4.set_title("Feedback weights")
-            ax5.set_title("Feedforward weights")
-            ax6.set_title("Test Accuracy")
-            ax7.set_title("Test Loss")
-
-            ax0.set_ylim(bottom=0)
-            ax1.set_ylim(bottom=0)
-            ax2.set_ylim(bottom=0)
-            ax3.set_ylim(bottom=0)
-            ax6.set_ylim(0, 1)
-            ax7.set_ylim(0, 1)
-
-            plt.savefig(os.path.join(imgdir, f"{epoch}.png"))
-            plt.close()
-
-            print(f"plot complete. epoch time: {np.mean(simulation_times[-50:]):.2f}s.\n")
             # print(f"test loss: {net.test_loss[-1][1]:.4f}")
             # print(f"ff error: {ff_error_now:.5f}, fb error: {fb_error_now:.5f}")
             # print(f"apical error: {apical_error_now:.2f}, intn error: {intn_error_now:.4f}\n")
@@ -246,11 +164,11 @@ finally:
         "test_acc": net.test_acc,
         "test_loss": net.test_loss,
         "train_loss": net.train_loss,
-        "ff_error": ff_error,
-        "fb_error": fb_error,
-        "V_ah_record": net.V_ah_record.tolist(),
-        "U_y_record": net.U_y_record.tolist(),
-        "U_i_record": net.U_i_record.tolist(),
+        # "ff_error": ff_error,
+        # "fb_error": fb_error,
+        # "V_ah_record": net.V_ah_record.tolist(),
+        # "U_y_record": net.U_y_record.tolist(),
+        # "U_i_record": net.U_i_record.tolist(),
         "epochs_completed": epoch
     }
     with open(os.path.join(root_dir, "progress.json"), "w") as f:
---
+++ pyramidal_microcircuit/plot_utils.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import utils
+import os
+from sklearn.metrics import mean_squared_error as mse
+
+
+ff_error = []
+fb_error = []
+
+
+def plot_progress(epoch, net, imgdir):
+    print(f"Ep {epoch}: generating plot...", end="")
+
+    cmap_1 = plt.cm.get_cmap('hsv', net.dims[1]+1)
+    cmap_2 = plt.cm.get_cmap('hsv', net.dims[2]+1)
+
+    plt.rcParams['savefig.dpi'] = 300
+
+    fig, axes = plt.subplots(4, 2, constrained_layout=True)
+    [ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7] = axes.flatten()
+
+    intn_error = np.square(net.U_y_record - net.U_i_record)
+
+    mean_error = utils.rolling_avg(np.mean(intn_error, axis=1), size=200)
+    ax0.plot(mean_error, color="black")
+
+    intn_error_now = np.mean(mean_error[-20:])
+    ax0_2 = ax0.secondary_yaxis("right")
+    ax0_2.set_yticks([intn_error_now])
+
+    apical_error = np.linalg.norm(net.V_ah_record, axis=1)
+    ax1.plot(utils.rolling_avg(apical_error, size=150))
+
+    apical_error_now = np.mean(apical_error[-20:])
+    ax1_2 = ax1.secondary_yaxis("right")
+    ax1_2.set_yticks([apical_error_now])
+
+    # Synaptic weights
+    # notice that all weights are scaled up again to ensure that derived metrics are comparible between simulations
+    weights = net.get_weight_dict()
+    WHI = weights[-2]["pi"]
+    WHY = weights[-2]["down"]
+    WIH = weights[-2]["ip"]
+    WYH = weights[-1]["up"]
+
+    fb_error_now = mse(WHY.flatten(), -WHI.flatten())
+    fb_error.append([epoch, fb_error_now])
+
+    ff_error_now = mse(WYH.flatten(), WIH.flatten())
+    ff_error.append([epoch, ff_error_now])
+
+    # ax2.plot(*zip(*fb_error), label=f"FB error: {fb_error_now:.3f}")
+    ax2.plot(*zip(*net.train_loss))
+    ax3.plot(*zip(*ff_error), label=f"FF error: {ff_error_now:.3f}")
+
+    # plot synaptic weights
+    for i in range(net.dims[2]):
+        col = cmap_2(i)
+        for j in range(net.dims[1]):
+            ax4.plot(j, -WHY[j, i], ".", color=col, label=f"to {i}")
+            ax4.plot(j, WHI[j, i], "x", color=col, label=f"from {i}")
+
+    for i in range(net.dims[1]):
+        for j in range(net.dims[2]):
+            col = cmap_2(j)
+            ax5.plot(i, WYH[j, i], ".", color=col, label=f"to {i}")
+            ax5.plot(i, WIH[j, i], "x", color=col, label=f"from {i}")
+
+    ax6.plot(*zip(*net.test_acc))
+    ax7.plot(*zip(*net.test_loss))
+
+    ax0.set_title("interneuron - pyramidal error")
+    ax1.set_title("apical error")
+    ax2.set_title("Feedback error")
+    ax3.set_title("Feedforward error")
+    ax4.set_title("Feedback weights")
+    ax5.set_title("Feedforward weights")
+    ax6.set_title("Test Accuracy")
+    ax7.set_title("Test Loss")
+
+    ax0.set_ylim(bottom=0)
+    ax1.set_ylim(bottom=0)
+    ax2.set_ylim(bottom=0)
+    ax3.set_ylim(bottom=0)
+    ax6.set_ylim(0, 1)
+    ax7.set_ylim(0, 1)
+
+    plt.savefig(os.path.join(imgdir, f"{epoch}.png"))
+    plt.close()
+    print(f"done.\n")
+
