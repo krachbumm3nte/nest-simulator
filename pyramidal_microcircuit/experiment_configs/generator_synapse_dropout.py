8e674a0fec5210e99cb07732255ac98dad0edd38
johannes gille <johannes.gille@gmx.de>
2023-03-26 13:45:59 +0200

    fix weight scale parameter study

--- pyramidal_microcircuit/experiment_configs/generator_synapse_dropout.py
+++ pyramidal_microcircuit/experiment_configs/generator_synapse_dropout.py
@@ -1,17 +1,18 @@
 import os
 import json
+import numpy as np
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 base_file = os.path.join(root_dir, "bars_le/bars_le_tpres_500.json")
-target_dir = os.path.join(root_dir, "weight_scale")
+target_dir = os.path.join(root_dir, "dropout")
 with open(base_file, "r") as f:
     config = json.load(f)
 
-weight_scale = [0.1, 0.25, 0.5, 1, 5, 10, 50, 100, 500, 1000]
+p_conn = np.linspace(0.6, 1.0, 5, endpoint=True)
 
 os.mkdir(target_dir)
-for scale in weight_scale:
-    config["weight_scale"] = scale
-    config_name = f"bars_le_weight_scale_{int(scale*100)}.json"
+for p in p_conn:
+    config["p_conn"] = p
+    config_name = f"dropout_p_{int(p*10)}.json"
     with open(os.path.join(target_dir, config_name), "w") as f:
         json.dump(config, f, indent=4)
