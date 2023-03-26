66890cd0f2799c1c191efe062a53f93254e4e98c
johannes gille <johannes.gille@gmx.de>
2023-03-26 14:46:17 +0200

    update weight_scale configs

--- pyramidal_microcircuit/experiment_configs/generator_weight_scale.py
+++ pyramidal_microcircuit/experiment_configs/generator_weight_scale.py
@@ -7,10 +7,11 @@ target_dir = os.path.join(root_dir, "weight_scale")
 with open(base_file, "r") as f:
     config = json.load(f)
 
-weight_scale = [0.1, 0.25, 0.5, 1, 5, 10, 50, 100, 500, 1000]
+weight_scale = [0.1, 0.25, 0.5, 1, 5, 10, 50, 100, 500, 1000, 5000]
 
 os.mkdir(target_dir)
 for scale in weight_scale:
+    config["dims"] = [9, 30, 3]
     config["weight_scale"] = scale
     config["network_type"] = "snest"
     config_name = f"bars_le_weight_scale_{int(scale*100)}.json"
