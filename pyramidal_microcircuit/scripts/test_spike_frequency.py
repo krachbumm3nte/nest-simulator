d6a5d7c83624157047a15606061aeb991a1b2952
johannes gille <johannes.gille@gmx.de>
2023-03-27 12:28:00 +0200

    add new figures

--- pyramidal_microcircuit/scripts/test_spike_frequency.py
+++ pyramidal_microcircuit/scripts/test_spike_frequency.py
@@ -1,24 +1,14 @@
-import argparse
-import json
-import os
-import sys
-import warnings
-from datetime import timedelta
-from time import time
-
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import src.utils as utils
-from src.networks.network_nest import NestNetwork
-from src.networks.network_numpy import NumpyNetwork
 from src.params import Params
-from src.plot_utils import plot_training_progress
 import nest
 
 params = Params()
 params.record_interval = 0.1
 params.threads = 1
-weight_scales = np.logspace(0, 1, 100, endpoint=True)
+weight_scales = np.logspace(-1, 1, 200, endpoint=True)
 
 
 acc = []
@@ -27,16 +17,16 @@ utils.setup_nest(params)
 params.setup_nest_configs()
 pyr = nest.Create(params.neuron_model, 1, params.pyr_params)
 intn = nest.Create(params.neuron_model, 1, params.intn_params)
-input_nrn = nest.Create(params.neuron_model, 1, params.input_params)
+#input_nrn = nest.Create(params.neuron_model, 1, params.input_params)
 
 sr = nest.Create("spike_recorder")
 nest.Connect(pyr, sr)
 nest.Connect(intn, sr)
-nest.Connect(input_nrn, sr)
+#nest.Connect(input_nrn, sr)
 
 pyr_hz = []
 intn_hz = []
-input_nrn_hz = []
+# input_nrn_hz = []
 sim_time = 1000
 
 
@@ -49,20 +39,32 @@ for scale in weight_scales:
     sr.n_events = 0
     pyr.gamma = params.gamma * scale
     intn.gamma = params.gamma * scale
-    input_nrn.gamma = scale
+    # input_nrn.gamma = scale
 
     pyr.set({"soma": {"I_e": 0.5 * params.g_som}})
     intn.set({"soma": {"I_e": 0.5 * params.g_som}})
-    input_nrn.set({"soma": {"I_e": 0.5 / params.tau_x}})
+    # input_nrn.set({"soma": {"I_e": 0.5 / params.tau_x}})
 
     nest.Simulate(sim_time)
 
     pyr_hz.append([scale, get_hz_from_sr(sr, pyr, sim_time)])
     intn_hz.append([scale, get_hz_from_sr(sr, intn, sim_time)])
-    input_nrn_hz.append([scale, get_hz_from_sr(sr, input_nrn, sim_time)])
+    # input_nrn_hz.append([scale, get_hz_from_sr(sr, input_nrn, sim_time)])
+
+
+min_freq = sorted(pyr_hz)[0]
+max_freq = sorted(pyr_hz)[-1]
+
+
+
+plt.axes().add_patch(patches.Rectangle([0.1, 200], 10, 600, facecolor="red", alpha = 0.3))
+
 
 plt.xscale("log")
-plt.plot(*zip(*sorted(pyr_hz)), label="pyr")
-plt.plot(*zip(*sorted(intn_hz)), label="intn")
-plt.plot(*zip(*sorted(input_nrn_hz)), label="input")
+plt.plot(*zip(*sorted(pyr_hz)), label="Pyramidal neuron")
+plt.plot(*zip(*sorted(intn_hz)), label="Interneuron")
+# plt.plot(*zip(*sorted(input_nrn_hz)), label="input")
+plt.xlabel("weight scale")
+plt.ylabel("mean firing rate [Hz]")
+plt.legend()
 plt.show()
