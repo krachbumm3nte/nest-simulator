b311fc8c18b4027fcd4940bdaec73d67f5074c4d
johannes gille <johannes.gille@gmx.de>
2023-04-05 15:25:45 +0200

    minor changes

---
+++ pyramidal_microcircuit/scripts/plot_progress.py
@@ -0,0 +1,57 @@
+import json
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import src.plot_utils as plot_utils
+import src.utils as utils
+from src.params import Params
+
+from src.networks.network_nest import NestNetwork
+from src.networks.network_numpy import NumpyNetwork
+
+
+if __name__ == "__main__":
+
+    plot_utils.setup_plt()
+
+    args = sys.argv[1:]
+
+    dirname = args[0]
+    target_dir = args[1]
+
+    all_configs = sorted(os.listdir(dirname))
+    fig, [ax0, ax1, ax2] = plt.subplots(3, 1)
+
+    final_performance = []
+    training_duration = []
+
+    p = Params(os.path.join(dirname, "params.json"))
+
+    with open(os.path.join(dirname, "progress.json")) as f:
+        progress = json.load(f)
+    with open(os.path.join(dirname, "weights.json")) as f:
+        weights = json.load(f)
+
+    if p.network_type == "numpy":
+        net = NumpyNetwork(p)
+    else:
+        net = NestNetwork(p)
+
+    net.set_all_weights(weights)
+
+    plot_utils.plot_training_progress()
+
+    weight_scale = params.weight_scale
+    acc = np.array(progress["test_acc"])
+
+
+    net.test_acc = progress["test_acc"]
+    net.test_loss = progress["test_loss"]
+    net.train_loss = progress["train_loss"]
+    net.ff_error = progress["ff_error"]
+    net.fb_error = progress["fb_error"]
+    net.epoch = progress["epochs_completed"]    
+
+    plot_utils.plot_training_progress(net.epoch, net, target_dir)
