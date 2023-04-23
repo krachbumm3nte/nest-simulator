d9bb6ae640b6b59d4042ffebbc8cf7fb3adb9895
johannes gille <johannes.gille@gmx.de>
2023-04-23 19:45:11 +0200

    remove apical compartment from simulation scripts

--- pyramidal_microcircuit/src/params.py
+++ pyramidal_microcircuit/src/params.py
@@ -106,11 +106,11 @@ class Params:
                 'g': self.g_a,
                 'C_m': self.C_m_api
             },
-            'apical_td': {
-                'g_L': self.g_lk_dnd if self.spiking else 1,
-                'g': self.g_a,
-                'C_m': self.C_m_api
-            },
+            # 'apical_td': {
+            #     'g_L': self.g_lk_dnd if self.spiking else 1,
+            #     'g': self.g_a,
+            #     'C_m': self.C_m_api
+            # },
             'lambda': self.g_som,  # Interneuron nudging conductance
             'gamma': self.gamma,
             'beta': self.beta,
