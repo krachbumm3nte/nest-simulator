fa824c33b2e870f59402d311dfc4fdf74040e717
johannes gille <johannes.gille@gmx.de>
2023-04-27 16:05:41 +0200

    update dropout parameter study

--- pyramidal_microcircuit/experiment_configs/generator_dropout.py
+++ pyramidal_microcircuit/experiment_configs/generator_dropout.py
@@ -4,14 +4,44 @@ import numpy as np
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 
-base_file = os.path.join(root_dir, "self_prediction/self_pred_snest.json")
-
 target_dir = os.path.join(root_dir, "par_study_dropout")
 p_conn = np.arange(0.1, 1, 0.2)
 
-with open(base_file, "r") as f:
-    config = json.load(f)
-
+config = {
+    "t_pres": 50,
+    "out_lag": 0,
+    "latent_equilibrium": True,
+    "weight_scale": 100,
+    "mode": "self-pred",
+    "init_self_pred": False,
+    "test_interval": -1,
+    "dims": [
+        6,
+        10,
+        3
+    ],
+    "n_epochs": 2000,
+    "store_errors": True,
+    "eta": {
+        "ip": [
+            0.02375,
+            0.0
+        ],
+        "pi": [
+            0.05,
+            0.0
+        ],
+        "up": [
+            0,
+            0
+        ],
+        "down": [
+            0.0,
+            0.0
+        ]
+    },
+    "network_type": "snest"
+}
 
 os.mkdir(target_dir)
 for p in p_conn:
