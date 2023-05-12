3fa3985f57208cf38105c60523cd2b179f338d77
johannes gille <johannes.gille@gmx.de>
2023-05-12 15:26:05 +0200

    update experiment configs

--- pyramidal_microcircuit/experiment_configs/generator_c_m.py
+++ pyramidal_microcircuit/experiment_configs/generator_c_m.py
@@ -4,7 +4,7 @@ import json
 root_dir = os.path.dirname(os.path.realpath(__file__))
 target_dir = os.path.join(root_dir, "par_study_c_m_weight_scale")
 
-weight_scale_list = [1, 5, 10]
+weight_scale_list = [10, 50, 100]
 c_m_list = [1, 10, 50]
 
 
@@ -41,6 +41,6 @@ for scale in weight_scale_list:
         config["weight_scale"] = scale
         config["network_type"] = "snest"
         config["C_m_api"] = c_m
-        config_name = f"bars_le_weight_scale_{int(scale*100)}_c_m_{int(10 * c_m)}.json"
+        config_name = f"bars_le_weight_scale_{scale}_c_m_{c_m}.json"
         with open(os.path.join(target_dir, config_name), "w") as f:
             json.dump(config, f, indent=4)
