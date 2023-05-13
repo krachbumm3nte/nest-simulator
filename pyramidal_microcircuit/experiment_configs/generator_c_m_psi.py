f4449d3d2241d3d5b8a26ce6d28ab7599523c0d4
johannes gille <johannes.gille@gmx.de>
2023-05-13 11:37:15 +0200

    update experiment configs

--- pyramidal_microcircuit/experiment_configs/generator_c_m_psi.py
+++ pyramidal_microcircuit/experiment_configs/generator_c_m_psi.py
@@ -1,5 +1,6 @@
 import os
 import json
+from copy import deepcopy
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 target_dir = os.path.join(root_dir, "par_study_c_m_psi")
@@ -8,7 +9,7 @@ psi_list = [10, 50, 100]
 c_m_list = [1, 10, 50]
 
 
-config = {
+default_config = {
     "t_pres": 200,
     "out_lag": 130.0,
     "latent_equilibrium": True,
@@ -31,16 +32,18 @@ config = {
             0.0
         ]
     },
-    "network_type": "snest"
+    "network_type": "snest",
+    "psi": 100,
+    "C_m_api": 1
 }
 
 
 os.mkdir(target_dir)
-for scale in psi_list:
+for psi in psi_list:
     for c_m in c_m_list:
-        config["psi"] = scale
-        config["network_type"] = "snest"
+        config = deepcopy(default_config)
+        config["psi"] = psi
         config["C_m_api"] = c_m
-        config_name = f"bars_le_psi_{scale}_c_m_{c_m}.json"
+        config_name = f"bars_le_psi_{psi}_c_m_{c_m}.json"
         with open(os.path.join(target_dir, config_name), "w") as f:
             json.dump(config, f, indent=4)
